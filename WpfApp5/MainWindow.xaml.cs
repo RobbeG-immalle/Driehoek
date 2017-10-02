d22661b17a47d8766ae0750ebb101a747fa7229a
robbe.gillis <robbe.gillis@B209-11>
2017-10-02 10:06:01 +0200

    driehoek

--- WpfApp5/MainWindow.xaml.cs
+++ WpfApp5/MainWindow.xaml.cs
@@ -24,5 +24,38 @@ namespace WpfApp5
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Line lijn = new Line();
+            lijn.X1 = 10;
+            lijn.Y1 = 10;
+            lijn.X2 = 10;
+            lijn.Y2 = 210;
+
+            lijn.Stroke = new SolidColorBrush(Colors.AliceBlue);
+            mijnCanvas.Children.Add(lijn);
+
+
+            Line lijn2 = new Line();
+            lijn2.X1 = 10;
+            lijn2.Y1 = 210;
+            lijn2.X2 = 210;
+            lijn2.Y2 = 100;
+
+            lijn2.Stroke = new SolidColorBrush(Colors.Aqua);
+            mijnCanvas.Children.Add(lijn2);
+
+            Line lijn3 = new Line();
+            lijn3.X1 = 210;
+            lijn3.Y1 = 100;
+            lijn3.X2 = 10;
+            lijn3.Y2 = 10;
+
+            lijn3.Stroke = new SolidColorBrush(Colors.Chocolate);
+            mijnCanvas.Children.Add(lijn3);
+
+
+        }
     }
 }
